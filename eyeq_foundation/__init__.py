d8e002cda5e9d7f47085f5595423bc2f8010d2a5
Hardik Dava <39372750+hardikdava@users.noreply.github.com>
2023-06-19 09:08:24 +0200

    Update __init__.py

--- eyeq_foundation/__init__.py
+++ eyeq_foundation/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.0.1"
 
 
 from eyeq_foundation.models.sam.sam import Sam, SamGuide
-from eyeq_foundation.models.grounding_dino.dino import Dino
+#from eyeq_foundation.models.grounding_dino.dino import Dino
 from eyeq_foundation.utils.annotator import Annotator
 from eyeq_foundation.utils.visualizer import visualizer
